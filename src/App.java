18f306ecd8d159dab69d5f427bb9ea4a88e82ae6
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-01-19 21:02:59 -0500

    added exclaimation mark to "programming is fun"

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun");
+        System.out.println("Programming is fun!");
     }
 }
