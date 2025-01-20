d5c92e2bdf6578b5347ba95fb91ca36368ef4919
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-01-19 20:18:00 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
